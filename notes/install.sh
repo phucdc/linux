847eadb126692b7679b3553c68445151b3bf828a
Duong Cong Phuc <82533607+phucdc@users.noreply.github.com>
2023-10-05 21:33:48 +0700

    idk

--- notes/install.sh
+++ notes/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function prerequisite() {
-    printf "=====[Prerequisite]=====\n'
+    printf "=====[Prerequisite]=====\n"
     apt-get update -y -qq &&
     apt-get install -y apt-utils autoconf automake build-essential git libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev -qq > /dev/null 2>&1 &&
     printf '-----[Done]-----\n\n'
 }
 
 function modsec3() {
-    printf "=====[Install modsecurity3 module]=====\n'
+    printf "=====[Install modsecurity3 module]=====\n"
     (git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity && cd ModSecurity
     git submodule init && git submodule update &&
     ./build.sh && mv ../ltmain.sh . ; ./build.sh && ./configure &&
@@ -17,7 +17,7 @@ function modsec3() {
 }
 
 function connector() {
-    printf "=====[Installing connector]=====\n'
+    printf "=====[Installing connector]=====\n"
     NGINX_VER=$(nginx -v 2>&1 | cut -d'/' -f2 | awk '{print $1}')
     git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git &&
     wget "http://nginx.org/download/nginx-${NGINX_VER}.tar.gz" &&
@@ -32,7 +32,7 @@ function connector() {
 }
 
 function configure() {
-    printf "=====[Configuring]=====\n'
+    printf "=====[Configuring]=====\n"
     [ ! -d /etc/nginx/modsec ] && mkdir /etc/nginx/modsec &&
     wget -P /etc/nginx/modsec/ https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended &&
     mv /etc/nginx/modsec/modsecurity.conf-recommended /etc/nginx/modsec/modsecurity.conf &&
@@ -43,7 +43,7 @@ function configure() {
 }
 
 function modsecurity_crs() {
-    printf "=====[Setting up modsecurity_crs]=====\n'
+    printf "=====[Setting up modsecurity_crs]=====\n"
     git clone https://github.com/coreruleset/coreruleset /usr/local/modsecurity-crs &&
     wget -P /usr/local/modsecurity-crs/ https://raw.githubusercontent.com/phucdc/linux/main/notes/crs-setup.conf &&
     mv /usr/local/modsecurity-crs/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.example /usr/local/modsecurity-crs/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf &&
@@ -51,7 +51,7 @@ function modsecurity_crs() {
 }
 
 function final() {
-    printf "=====[Final step]=====\n'
+    printf "=====[Final step]=====\n"
     wget https://raw.githubusercontent.com/phucdc/linux/main/notes/default &&
     mv default /etc/nginx/sites-available/default && mkdir /usr/local/modsec/ &&
     wget -P /usr/local/modsec/ https://raw.githubusercontent.com/phucdc/linux/main/notes/custom.conf &&
